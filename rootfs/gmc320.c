5abdb56a860977532222f422968d83a98f128b0a
Jasper Hugo <jasper@jasperhugo.com>
2025-11-06 13:02:12 +0100

    add missing include, remove unnecessary comments

--- rootfs/gmc320.c
+++ rootfs/gmc320.c
@@ -4,12 +4,13 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h> // Contains file controls like O_RDWR
-#include <errno.h> // Error integer and strerror() function
-#include <termios.h> // Contains POSIX terminal control definitions
-#include <unistd.h> // write(), read(), close()
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
 #include <time.h>
 #include <inttypes.h>
+#include <stdlib.h>
 
 int serial_port;
 
